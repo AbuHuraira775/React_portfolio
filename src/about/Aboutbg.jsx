b9678e5c8eb7d104fc8956d19d8cd85992ebb2db
Abu Huraira <abuhurairah775@gmail.com>
2023-05-25 13:57:00 +0500

    Updates

--- src/about/Aboutbg.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Aboutbg() {
-  return (
-    <div className='aboutbg'>
-   
-    </div>
-  )
-}
--- src/portfolio/PortBG.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PortBG() {
-  return (
-    <div className='portfolio_bg'>
-       
-    </div>
-  )
-}
